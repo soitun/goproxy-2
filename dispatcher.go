ab155fe612cb5e7cd7d1282438946a410a548eda
haimingZZ <haiming@haimingzz.com>
2026-09-23 00:20:50 +0800

    docs: fix typo 'wether' -> 'whether' in dispatcher.go comments

--- dispatcher.go
+++ dispatcher.go
@@ -43,7 +43,7 @@ func (c RespConditionFunc) HandleResp(resp *http.Response, ctx *ProxyCtx) bool {
 	return c(resp, ctx)
 }
 
-// UrlHasPrefix returns a ReqCondition checking wether the destination URL the proxy client has requested
+// UrlHasPrefix returns a ReqCondition checking whether the destination URL the proxy client has requested
 // has the given prefix, with or without the host.
 // For example UrlHasPrefix("host/x") will match requests of the form 'GET host/x', and will match
 // requests to url 'http://host/x'
@@ -132,7 +132,7 @@ func UrlMatches(re *regexp.Regexp) ReqConditionFunc {
 	}
 }
 
-// DstHostIs returns a ReqCondition testing wether the host in the request url is the given string.
+// DstHostIs returns a ReqCondition testing whether the host in the request url is the given string.
 func DstHostIs(host string) ReqConditionFunc {
 	// Make sure to perform a case-insensitive host check
 	host = strings.ToLower(host)
